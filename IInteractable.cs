529abba033d8e914915715d02f6e78507c59d704
tajgerek1 <m.nazarko.tp22@zsme.tarnow.pl>
2026-05-04 17:41:47 +0200

    Delete IInteractable.cs

--- IInteractable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NightWatch.Interaction
-{
-    public interface IInteractable
-    {
-        string GetInteractionPrompt(PlayerInteractor interactor);
-        void Interact(PlayerInteractor interactor);
-    }
-}
